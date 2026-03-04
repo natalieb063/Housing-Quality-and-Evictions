37aa5b220c40b3dd50bf3addd880573f9427116e
Natalie Brown <55594673+natalieb063@users.noreply.github.com>
2026-03-04 15:32:45 -0500

    Create script for cleaning HPD jurisdiction data
    This script cleans and processes HPD jurisdiction data, filtering out demolished buildings and categorizing BINs. It also merges geocoded data and prepares the final output for analysis.

---
+++ 03_cleaning_HPD_Juris.R
@@ -0,0 +1,103 @@
+###############################################################################
+### PROGRAM NAME:  EVICTIONS - 2026                                         ###
+### DESCRIPTION:   Cleaning HPD Jurisdiction                                ###
+### ANALYST:       Natalie Brown                                            ###
+### DATE:          1/20/2026                                                ###
+###############################################################################
+
+options(scipen = 999)
+rm(list = ls())
+
+#LIBRARIES######################################################################
+
+library(stringr)
+library(dplyr)
+library(haven)
+library(tidyverse)
+library(skimr)
+library(data.table)
+library(reshape2)
+
+#PATHS & VARS###################################################################
+
+hpd_juris_path <- 
+output.path <- 
+
+#DATA##########################################################################
+
+hpd_juris <- fread(paste0(hpd_juris_path, 'Buildings_Subject_to_HPD_Jurisdiction_20260202.csv')) %>%
+  select(-c(ManagementProgram, DoBBuildingClassID, DoBBuildingClass, LegalStories,
+            LegalClassB, RegistrationID, RecordStatusID, RecordStatus)) %>%
+  filter(LifeCycle != "Demolished")
+  
+weird_bins_hpdjuris <- hpd_juris %>%
+  filter(BIN == 1000000 | BIN == 2000000| BIN == 3000000 |BIN == 4000000 |BIN == 5000000 | is.na(BIN) | BIN == 0)
+
+not_weird_bins_hpdjuris <- hpd_juris %>%
+  filter(BIN != 1000000 & BIN != 2000000 & BIN != 3000000 & BIN != 4000000 & BIN != 5000000 & !is.na(BIN) | BIN != 0)
+
+#write.csv(weird_bins_hpdjuris, "hpd_juris_add.csv")
+#write.csv(not_weird_bins_hpdjuris, "hpd_juris_BIN.csv")
+
+####BRINGING BACK GEOCODED DATA         ########################################
+
+geocoded_path <-
+
+#hpd_juris_bins <- fread(paste0(geocoded_path, 'hpd_juris_BIN_SUCCESS.csv')) %>% 
+#  select(-c(V1:V4, V7:V8, V11:V12, V14:V17))
+#hpd_juris_add1 <- fread(paste0(geocoded_path, 'hpd_juris_BIN_attempt2_add_SUCCESS_E.csv')) %>%
+#  select(-c(V1:V4, V7:V8, V11:V12, V14:V17))
+hpd_juris_add2 <- fread(paste0(geocoded_path, 'hpd_juris_add_SUCCESS_E.csv')) %>%
+  select(-c(V1:V4, V7:V8, V11:V12, V14:V17))
+
+new_names_hpd_juris_bins <- c("borough", "house_number", "street", "zipcode",
+                         "BIN_old", "BIN_new", "x", "y")
+
+#colnames(hpd_juris_bins) <- new_names_hpd_juris_bins
+#colnames(hpd_juris_add1) <- new_names_hpd_juris_bins
+colnames(hpd_juris_add2) <- new_names_hpd_juris_bins
+
+#hpd_juris_bins <- hpd_juris_bins %>% 
+#  mutate(x = as.integer(x),
+#         y = as.integer(y))
+
+junkbins=c(1000000,2000000,3000000,300000,4000000,5000000,0) #NB: added in 0 and 300000
+
+hpd_juris_updated=left_join(hpd_juris,hpd_juris_add2, by = join_by(HouseNumber == house_number,
+                                                                   StreetName == street,
+                                                                   Zip == zipcode,
+                                                                   BIN == BIN_old,
+                                                                   Boro == borough)) %>%
+  select(-c(x, y)) %>%
+  mutate(BIN=ifelse(!is.na(BIN_new), BIN_new, BIN)) %>%
+  filter(BIN %in% junkbins==F & !is.na(BIN))
+
+#sizes <- hpd_juris %>% select(BIN, LegalClassA) #bringing in units per building
+
+#hpd_juris_full <- bind_rows(hpd_juris_bins, hpd_juris_add1, hpd_juris_add2) %>% 
+#  distinct() %>%
+#  mutate(full_address = case_when(house_number == 0 ~ paste0(street),
+#                                  house_number != 0 ~ paste0(house_number, " ", street))) %>%
+#  left_join(sizes, by = join_by(BIN_new == BIN)) %>%
+#  rename(BIN = BIN_new,
+#         n_units = LegalClassA)
+
+#hpd_juris_output <- hpd_juris_full %>%
+#  select(BIN, full_address, x, y, n_units) %>%
+#  distinct(BIN, .keep_all = TRUE)
+
+hpd_juris_output <- hpd_juris_updated %>%
+  mutate(full_address = case_when(HouseNumber == 0 ~ paste0(StreetName),
+                                  HouseNumber != 0 ~ paste0(HouseNumber, " ", StreetName))) %>%
+  rename(n_units = LegalClassA) %>%
+  select(BIN, full_address, Boro, Zip, n_units) %>%
+  distinct(BIN, .keep_all=T)
+
+hpd_juris_output %>%
+   group_by(BIN) %>%
+   summarise(n = n()) %>%
+   arrange(desc(n))
+
+
+write.csv(hpd_juris_output, paste0(output.path,'hpdjuris_by_BIN.csv'), row.names = F)
+         
