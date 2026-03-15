5705e8afc1710ec631195610e6b27ec5316ed677
Natalie Brown <natalieb063@gmail.com>
2026-03-15 18:30:21 -0400

    Update 02_cleaning_violations.R

--- 02_cleaning_violations.R
+++ 02_cleaning_violations.R
@@ -24,17 +24,13 @@ violations_path <- '/Users/nataliebrown/Desktop/housing_quality_nyc/violations/'
 
 #DATA##########################################################################
 
-violations_2019 <- read.csv(paste0(violations_path, 'violations_2019.csv'))
-violations_2020 <- read.csv(paste0(violations_path, 'violations_2020.csv'))
-violations_2021 <- read.csv(paste0(violations_path, 'violations_2021.csv'))
-violations_2022 <- read.csv(paste0(violations_path, 'violations_2022_premoratorium.csv'))
+violations_2022_2025 <- read.csv(paste0(violations_path, 'violations_2022_2025.csv'))
+violations_2014_2018 <- read.csv(paste0(violations_path, 'violations_2014_2018.csv'))
 
-list_of_violations_dfs <- list(violations_2019 = violations_2019,
-                               violations_2020 = violations_2020,
-                               violations_2021 = violations_2021,
-                               violations_2022 = violations_2022)
+list_of_violations_dfs <- list(violations_2014_2018 = violations_2014_2018,
+                               violations_2022_2025 = violations_2022_2025)
 
-cols_to_keep <- c("ViolationID","Borough", "Class", "HouseNumber", "StreetName", "Postcode",
+cols_to_keep <- c("ViolationID", "Borough", "Class", "HouseNumber", "StreetName", "Postcode",
                   "InspectionDate", "BIN")
 
 cleaned_violations_dfs <- lapply(list_of_violations_dfs, function(df) {
@@ -43,7 +39,7 @@ cleaned_violations_dfs <- lapply(list_of_violations_dfs, function(df) {
 
 list2env(cleaned_violations_dfs, envir = .GlobalEnv)
 
-violations_combined <- bind_rows(violations_2019, violations_2020, violations_2021, violations_2022) %>%
+violations_combined <- bind_rows(violations_2014_2018, violations_2022_2025) %>%
   mutate(viol_date = as.Date(InspectionDate, "%m/%d/%Y"),
          viol_year = year(viol_date))
 
@@ -56,16 +52,16 @@ junk_bin <- c(1000000, 2000000, 3000000, 4000000, 5000000)
 junk_bins <- violations_combined %>%
   filter(BIN %in% junk_bin | is.na(BIN))
 
-write.csv(junk_bins, "viol_junk_bins_6mar2026.csv")
+write.csv(junk_bins, "viol_junk_bins_13mar2026.csv")
 
 ####BRINGING BACK GEOCODED DATA         ########################################
 
 geocoding_folder <- "/Users/nataliebrown/Desktop/housing_quality_nyc/outputs/geocoded/"
 
-violations_by_add <- fread(paste0(geocoding_folder, 'viol_junk_bins_SUCCESS_E.csv')) %>%
-  select(-c(V1:V2, V10))
+violations_by_add <- fread(paste0(geocoding_folder, 'viol_junk_bins_13mar2026_SUCCESS_E.csv')) %>%
+  select(-c(V1, V10))
 
-new_names_by_add<- c("ViolationID", "borough", "house_number","street", "zipcode",
+new_names_by_add<- c("ViolationID", "borough", "class", "house_number","street", "zipcode",
                      "inspection_date", "BIN_old", "viol_year", "BIN_new")
 
 colnames(violations_by_add) <- new_names_by_add
@@ -87,5 +83,5 @@ viol_year[is.na(viol_year)]=0
 viol_year <- viol_year %>%
   mutate(viol_total = sum(A,B,C,I))
 
-write.csv(viol_year, 'viol_by_year_6mar2026.csv', row.names=F)
+write.csv(viol_year, 'viol_by_year_13mar2026.csv', row.names=F)
 
